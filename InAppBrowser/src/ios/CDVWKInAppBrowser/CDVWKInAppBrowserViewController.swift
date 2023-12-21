5d6a90a2b735823cc9b64f59532d423f041472df
kaesyar <136590661+kaesyar@users.noreply.github.com>
2023-12-21 21:43:48 +0500

    fix options parsing

--- InAppBrowser/src/ios/CDVWKInAppBrowser/CDVWKInAppBrowserViewController.swift
+++ InAppBrowser/src/ios/CDVWKInAppBrowser/CDVWKInAppBrowserViewController.swift
@@ -267,7 +267,7 @@ extension CDVWKInAppBrowserViewController {
         webView = WKWebView(frame: webViewBounds, configuration: configuration)
         
         view.addSubview(webView)
-        view.sendSubview(toBack: webView)
+        view.sendSubviewToBack(webView)
         
         webView.navigationDelegate = self
         webView.uiDelegate = webViewUIDelegate
@@ -290,7 +290,7 @@ extension CDVWKInAppBrowserViewController {
     }
     
     func setupSpinner() {
-        spinner = UIActivityIndicatorView(activityIndicatorStyle: .medium)
+        spinner = UIActivityIndicatorView(style: .medium)
         spinner.alpha = 1.000
         spinner.autoresizesSubviews = true
         spinner.autoresizingMask = [.flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin, .flexibleRightMargin]
